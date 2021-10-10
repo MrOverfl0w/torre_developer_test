966440dbb4256ac5f905d7028f8bf7739e11e782
MrOverfl0w <alberto_pumarejo@hotmail.com>
2021-10-10 15:39:21 -0500

    List search results
    Fetch users and filter mentors based on skill selected

--- backend/server.go
+++ backend/server.go
@@ -1,31 +1,80 @@
-package backend
+package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+
+	"github.com/Jeffail/gabs"
 )
 
 // Input
 type skillSearch struct {
-	Skill string 'json:"skill"'
+	Skill string `json:"skill"`
 }
 
 // Output
 type listItemData struct {
-	Name string 'json:"name"'
-	Price int64 'json:"price"'
-	Remote bool 'json:"remote"'
-	Location string 'json:"location"'
+	Name     string  `json:"name"`
+	Price    float64 `json:"price"`
+	Remote   bool    `json:"remote"`
+	Location string  `json:"location"`
 }
 
 type listResponseData struct {
-	Results []listItemData 'json:"results"'
+	Results []listItemData `json:"results"`
 }
 
 // Process Request for index results
-func process(requestData skillSearch) (listResponseData) {
+func process(requestData skillSearch) listResponseData {
 	var listResponse listResponseData
+
+	usersjson, err := http.Post("https://search.torre.co/people/_search/?offset=0&size=1000", "", nil)
+	if err != nil {
+		log.Fatalf("Error in API: %v", err)
+	}
+	defer usersjson.Body.Close()
+	jsonResponse, err := ioutil.ReadAll(usersjson.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	jsonParsed, _ := gabs.ParseJSON([]byte(jsonResponse))
+	children, _ := jsonParsed.Path("results").Children()
+	//Children Array from Results
+	for _, child := range children {
+		mentor := false
+		childrenOpen, _ := child.Path("openTo").Children()
+		for _, childOpen := range childrenOpen {
+			if childOpen.Data().(string) == "mentoring" {
+				mentor = true
+			}
+		}
+
+		if mentor {
+			var weight float64
+			weight = 0
+			children2, _ := child.Path("skills").Children()
+			//Children Array from Skills
+			for _, child2 := range children2 {
+				if child2.Path(("name")).Data() == requestData.Skill {
+					weight = child2.Path(("weight")).Data().(float64)
+				}
+			}
+			if weight > 0 {
+				var itemDataResponse listItemData
+				itemDataResponse.Name = child.Path("name").Data().(string)
+				itemDataResponse.Price = child.Path("compensations").Path("employee").Path("minHourlyUSD").Data().(float64)
+				itemDataResponse.Remote = child.Path("remoter").Data().(bool)
+				itemDataResponse.Location = child.Path("locationName").Data().(string)
+
+				listResponse.Results = append([]listItemData{itemDataResponse})
+			}
+		}
+
+	}
+
 	return listResponse
 }
 
@@ -36,10 +85,11 @@ func getResponse(w http.ResponseWriter, request *http.Request) {
 	var skill skillSearch
 	var response listResponseData
 
-	decode.Decode(&skill)
+	decoder.Decode(&skill)
+	log.Println(skill.Skill)
 
 	response = process(skill)
-	
+
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(http.StatusOK)
@@ -49,7 +99,7 @@ func getResponse(w http.ResponseWriter, request *http.Request) {
 	}
 }
 
-func main(){
+func main() {
 	http.HandleFunc("/", getResponse)
 	http.ListenAndServe(":8090", nil)
 }
